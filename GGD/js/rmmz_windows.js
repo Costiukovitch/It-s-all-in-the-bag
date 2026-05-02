a0e03d89d60d8326fda424daed338759a8ef1aa5
emeraccco <emeraccco@gmail.com>
2026-05-02 18:00:02 +0500

    Изменил Меню

--- GGD/js/rmmz_windows.js
+++ GGD/js/rmmz_windows.js
@@ -1653,7 +1653,7 @@ Window_Gold.prototype.refresh = function() {
     const y = rect.y;
     const width = rect.width;
     this.contents.clear();
-    this.drawCurrencyValue(this.value(), this.currencyUnit(), x, y, width);
+  //  this.drawCurrencyValue(this.value(), this.currencyUnit(), x, y, width);
 };
 
 Window_Gold.prototype.value = function() {
@@ -1747,8 +1747,8 @@ Window_StatusBase.prototype.placeTimeGauge = function(actor, x, y) {
 };
 
 Window_StatusBase.prototype.placeBasicGauges = function(actor, x, y) {
-    this.placeGauge(actor, "hp", x, y);
-    this.placeGauge(actor, "mp", x, y + this.gaugeLineHeight());
+  //  this.placeGauge(actor, "hp", x, y);
+  //  this.placeGauge(actor, "mp", x, y + this.gaugeLineHeight());
     if ($dataSystem.optDisplayTp) {
         this.placeGauge(actor, "tp", x, y + this.gaugeLineHeight() * 2);
     }
@@ -1809,11 +1809,11 @@ Window_StatusBase.prototype.drawActorIcons = function(actor, x, y, width) {
 Window_StatusBase.prototype.drawActorSimpleStatus = function(actor, x, y) {
     const lineHeight = this.lineHeight();
     const x2 = x + 180;
-    this.drawActorName(actor, x, y);
-    this.drawActorLevel(actor, x, y + lineHeight * 1);
-    this.drawActorIcons(actor, x, y + lineHeight * 2);
-    this.drawActorClass(actor, x2, y);
-    this.placeBasicGauges(actor, x2, y + lineHeight);
+  //  this.drawActorName(actor, x, y);
+  //  this.drawActorLevel(actor, x, y + lineHeight * 1);
+  //  this.drawActorIcons(actor, x, y + lineHeight * 2);
+  //  this.drawActorClass(actor, x2, y);
+  //  this.placeBasicGauges(actor, x2, y + lineHeight);
 };
 
 Window_StatusBase.prototype.actorSlotName = function(actor, index) {
